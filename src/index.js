6b780f5a0eb4efcf3e7a0679edacfecc30cbfcf3
vcaquant <vcaquant@student.42.fr>
2017-05-22 19:50:23 +0200

    auto commit

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { render } from 'react-dom';
+// CSS
+import './style/css/bootstrap.min.css';
+import './index.css';
+// Sample
+import { sampleText } from './sampleText';
+// Marked
+import marked from 'marked';
+
+class App extends React.Component {
+
+    state = {
+        text: sampleText
+    };
+
+    componentWillMount() {
+        const text = localStorage.getItem('text');
+        if (text) {
+            this.setState({ text });
+        }
+
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        localStorage.setItem('text', nextState.text);
+    }
+
+    editText = (event) => {
+        const text = event.target.value;
+        this.setState({ text });
+
+    };
+
+    renderText = (text) => {
+        const renderText = marked(text, {sanitize: true});
+        return { __html: renderText };
+    };
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <textarea
+                            value={this.state.text}
+                            className="form-control"
+                            rows="25"
+                            onChange={(e) => this.editText(e)}>
+                        </textarea>
+                    </div>
+                    <div className="col-sm-6">
+                        <div
+                            dangerouslySetInnerHTML={this.renderText(this.state.text)}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+render(
+    <App />,
+    document.getElementById('root')
+);
